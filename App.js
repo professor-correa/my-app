545c8c6115a932414cb02f0a658804a79ffa2872
lucascorreaa <lucas.lca14@gmail.com>
2026-08-12 10:19:54 -0300

    feat: create page init

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
+    // TODO: Colocar um icone, um title e dois botões em linha cadastre e login com estilo diferentes
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Importação com require, lembra do commonjs */}
+      {/* Estilo em linha -> chaves duplas */}
+      <Image source={require("./assets/icon.png")} style={{width: 120, height: 120}} />
+      <Text style={styles.title}>App Title</Text>
+      <View style={styles.btnWrapper}>
+        {/* Vizualização no terminal, é um console... */}
+        <Pressable onPress={() => console.log("Pressable")}>
+          <Text style={styles.btnPrimary} >Não tem cadastro</Text>
+        </Pressable>
+        <TouchableOpacity style={styles.btnSecondary} onPress={() => console.log("TouchableOpacity")}>
+          <Text style={{color: "white"}}>Login</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 32
   },
+  title: {
+    fontSize: 52
+  },
+  btnWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+    gap: 24
+  }, // ele empacota os botões e deixa em linha.
+  btnPrimary: {
+    textDecorationLine: "underline",
+    color: "black"
+  }, // esse tem estilo com cor e underline
+  btnSecondary: {
+    backgroundColor: "blue",
+    borderRadius: 16,
+    padding: 10,
+  }, // esse tem cor de fundo e bordas...
 });
