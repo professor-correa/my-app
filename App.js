7cdf960e18486beb9838be6053dcbd86aea1d3f7
lucascorreaa <lucas.lca14@gmail.com>
2026-08-25 10:59:39 -0300

    feat(navigation): add root stack for application

--- App.js
+++ App.js
@@ -1,53 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+import { FPassword, SignIn, SignUp } from "./src/screens"
+
+const Stack = createNativeStackNavigator()
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="ForgotPassword" component={FPassword} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
-  return (
-    // TODO: Colocar um icone, um title e dois botões em linha cadastre e login com estilo diferentes
-    <View style={styles.container}>
-      {/* Importação com require, lembra do commonjs */}
-      {/* Estilo em linha -> chaves duplas */}
-      <Image source={require("./assets/icon.png")} style={{width: 120, height: 120}} />
-      <Text style={styles.title}>App Title</Text>
-      <View style={styles.btnWrapper}>
-        {/* Vizualização no terminal, é um console... */}
-        <Pressable onPress={() => console.log("Pressable")}>
-          <Text style={styles.btnPrimary} >Não tem cadastro</Text>
-        </Pressable>
-        <TouchableOpacity style={styles.btnSecondary} onPress={() => console.log("TouchableOpacity")}>
-          <Text style={{color: "white"}}>Login</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+  return ( 
+   <NavigationContainer>
+      <RootStack />
+   </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     gap: 32
   },
-  title: {
-    fontSize: 52
-  },
-  btnWrapper: {
-    justifyContent: "center",
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "row",
-    gap: 24
-  }, // ele empacota os botões e deixa em linha.
-  btnPrimary: {
-    textDecorationLine: "underline",
-    color: "black"
-  }, // esse tem estilo com cor e underline
-  btnSecondary: {
-    backgroundColor: "blue",
-    borderRadius: 16,
-    padding: 10,
-  }, // esse tem cor de fundo e bordas...
 });
--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-import { FPassword, SignIn, SignUp } from "./src/pages"
-
-
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(SignIn);
+registerRootComponent(App);
