4897ad513a6cb798cbe429553ce4e29818d2e951
lucascorreaa <lucas.lca14@gmail.com>
2026-08-26 11:12:32 -0300

    feat: add a new stack group to the details modal, but an error occurred during the mobile transition

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import { FPassword, SignIn, SignUp } from "./src/screens"
+import DetailsModal from './src/components/modal';
 
 const Stack = createNativeStackNavigator()
 
@@ -13,6 +14,13 @@ const RootStack = () => {
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="ForgotPassword" component={FPassword} />
+      <Stack.Group screenOptions={{ 
+        presentation: "transparentModal", animation: "slide_from_bottom" }}>
+        <Stack.Screen
+          name='DetailsModal' 
+          component={DetailsModal} 
+          options={{ headerShown: false }} />
+      </Stack.Group>
     </Stack.Navigator>
   )
 }
--- src/screens/forgot-password/index.jsx
+++ src/screens/forgot-password/index.jsx
@@ -1,7 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
 import { Btn, BtnTitle, Container, Title } from "./style"
 
-
 const ForgotPassword = () => {
     
     const navigation = useNavigation()
@@ -12,6 +11,10 @@ const ForgotPassword = () => {
             <Btn 
                 onPress={() => navigation.navigate("SignIn")}>
                 <BtnTitle>Sign In</BtnTitle>
+            </Btn>
+            <Btn 
+                onPress={() => navigation.navigate("DetailsModal")}>
+                <BtnTitle>Details Modal</BtnTitle>
             </Btn>
         </Container>
     )
