227c10adfad3f5d1b631f0ac46c66ea2cd382ac1
lucascorreaa <lucas.lca14@gmail.com>
2026-09-01 10:44:48 -0300

    refactor(modal): add icons and new layout

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,16 +1,27 @@
 // namespace import
 import * as DM from "./style"
 import { useNavigation } from "@react-navigation/native"
+import { FontAwesome6 } from "@expo/vector-icons"
 
 const DetailsModal = () => {
     const navigation = useNavigation()
     return (
         <DM.Overlay onPress={() => navigation.goBack()}>
             <DM.Container>
+                <DM.IconCircle>
+                    <FontAwesome6 
+                        name="check"
+                        size={32}
+                        color="white"
+                    />
+                </DM.IconCircle>
                 <DM.Title>Title</DM.Title>
                 <DM.Description>DMescription</DM.Description>
                 <DM.CloseBtn onPress={() => navigation.goBack()}>
-                    <DM.CloseBtnText>Close</DM.CloseBtnText>
+                    <FontAwesome6 
+                        name="xmark"
+                        size={24}
+                    />
                 </DM.CloseBtn>
             </DM.Container>
         </DM.Overlay>
--- src/components/modal/style.js
+++ src/components/modal/style.js
@@ -7,17 +7,41 @@ const Overlay = styled.TouchableOpacity`
 
 const Container = styled.View`
     height: 50%;
-    background-color: blue;
+    background-color: white;
     border-top-left-radius: 24px;
     border-top-right-radius: 24px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding-top: 64px;
+    gap: 24px; 
+`
+
+const IconCircle = styled.View`
+    width: 72px;
+    height: 72px;
+    border-radius: 72px;
+    background-color: green;
 
     display: flex;
     justify-content: center;
     align-items: center;
 `
-const Title = styled.Text``
-const Description = styled.Text``
-const CloseBtn = styled.TouchableOpacity``
-const CloseBtnText = styled.Text``
 
-export { CloseBtn, CloseBtnText, Container, Description, Title, Overlay }
+const Title = styled.Text`
+    font-size: 32px;
+`
+
+const Description = styled.Text`
+    font-size: 16px;
+`
+
+const CloseBtn = styled.TouchableOpacity`
+    position: absolute;
+    top: 24px;
+    right: 24px;
+`
+
+export { CloseBtn, Container, Description, IconCircle, Title, Overlay }
