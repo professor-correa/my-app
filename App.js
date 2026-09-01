7ac5fba06579de18c8639f98c92c1c2dd865a11c
lucascorreaa <lucas.lca14@gmail.com>
2026-09-01 09:52:06 -0300

    chore(app): add path aliases

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
-import { FPassword, SignIn, SignUp } from "./src/screens"
-import DetailsModal from './src/components/modal';
+import { FPassword, SignIn, SignUp } from "@screens/"
+import DetailsModal from '@components/modal';
 
 const Stack = createNativeStackNavigator()
 
